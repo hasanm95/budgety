72e20b8d3ff88305edeae3fe686792e607fd11c2
Hasan <hasanm025@yahoo.com>
2018-12-07 20:46:25 +0600

    Setup Click and Keypress Event Listener

--- app.js
+++ app.js
@@ -14,4 +14,43 @@ var UIController = (function(){
 
 var controller = (function(budgetCtrl, UICtrl){
 
+    var ctrlAddItem = function(){
+        // 1. Get input Data
+
+        // 2. Add the item to the Budget Controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate Budget
+
+        // 5. Display the budget on UI
+    };
+
+
+
+    // Setup event handler
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        ctrlAddItem();
+    });
+
+    // Setup Keypress Event
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode == 13 || event.which == 13){
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
+
+
+
+
+
+
+
+
+
+
+
+
