59410814299f69dcbf74bded5223dd7d685d0537
Hasan <hasanm025@yahoo.com>
2018-12-08 23:08:17 +0600

    Added item to data structure

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 // Budget Controller
 var budgetController = (function(){
 
+    // Expense Constructor
     var Expense = function(id, description, value){
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
+    // Income Constructor
     var Income = function(id, description, value){
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
+    // Data Structure
     var data = {
         allItems: {
             inc: [],
@@ -23,13 +26,63 @@ var budgetController = (function(){
             exp: 0
         }
     };
+
+    // Public methods to inherit form other modules
+    return {
+        addItem: function(type, desc, val){
+            var newItem, ID, dataType;
+
+            dataType = data.allItems[type];
+
+            // ID = [1,2,3,4] next ID = 5
+            // ID = [1,2,7,9] next ID = 10
+            // ID = last id + 1
+
+            // Create new ID
+            if(dataType.length > 0){
+                ID = dataType[dataType.length - 1].id + 1;
+            }else{
+                ID = 0;
+            }
+
+            // Create new item based on Inc or Exp
+            if(type === 'exp'){
+                newItem = new Expense(ID, desc, val);
+            }else{
+                newItem = new Income(ID, desc, val);
+            }
+
+            // Push it into our data structure
+            dataType.push(newItem);
+            
+            // Return the new Element
+            return newItem;
+        },
+        testing: function(){
+            console.log(data);
+        }
+        
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })();
 
 // UI Controller
 var UIController = (function(){
 
     // DOM Strings
-
     var DOMStrings = {
         inputType: '.add__type',
         inputDesc: '.add__description',
@@ -38,7 +91,6 @@ var UIController = (function(){
     };
 
     // Public object
-
     return{
         getInput: function(){
             return{
@@ -76,11 +128,12 @@ var controller = (function(budgetCtrl, UICtrl){
     };
 
     var ctrlAddItem = function(){
+        var input, newItem
         // 1. Get input Data
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
 
         // 2. Add the item to the Budget Controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
